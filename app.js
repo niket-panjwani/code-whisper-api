f648bcd23246a042ea7957f1823f939505343500
niket_panjwani@mckinsey.com <niket_panjwani@mckinsey.com>
2024-03-15 22:52:09 -0500

    Adding port binding

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 const openai = new OpenAI({ apiKey: process.env.OPEN_AI_API_KEY });
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
